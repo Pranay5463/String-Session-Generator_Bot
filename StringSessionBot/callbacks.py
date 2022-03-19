5451d23f4e1963c8eefaa46d8f3de3f3cc4d743a
Pranay Prangan <86947553+Pranay5463@users.noreply.github.com>
2022-03-20 00:17:30 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -65,6 +65,6 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
 
 
 ERROR_MESSAGE = "Oops! An exception occurred! \n\n**Error** : {} " \
-            "\n\nPlease visit @StarkBotsChat if this message doesn't contain any " \
+            "\n\nPlease visit @NotrealPranay if this message doesn't contain any " \
             "sensitive information and you if want to report this as " \
             "this error message is not being logged by us!"
