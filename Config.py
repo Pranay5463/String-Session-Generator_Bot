bff9c331bf1a1b376a837d21bcb90f22e74edccc
Vikrant Ojha <daredevil.vikrant@gmail.com>
2022-05-14 15:44:24 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,19 +9,7 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN.replace("@", "")
-else:
-    # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN[1:]
+    MONGO_URI = os.environ.get('MONGO_URI', None)
+    UPDATES_CHANNEL = os.environ.get('UPDATES_CHANNEL', None)
+    if UPDATES_CHANNEL.startswith("@"):
+        UPDATES_CHANNEL = UPDATES_CHANNEL[1:]
