ef3ec528d58f8c3b51f2f9528bde9a2fbc769dcd
Vikrant Ojha <daredevil.vikrant@gmail.com>
2022-05-14 16:10:53 +0530

    Update commands.py

--- Flux/commands.py
+++ Flux/commands.py
@@ -4,13 +4,22 @@ from pyrogram.types import InlineKeyboardMarkup
 from .database.mongo import insert, find_one
 
 @Client.on_message(filters.private & filters.incoming & filters.command("about"))
-async def about(bot, msg):
-    await bot.send_message(
-        msg.chat.id,
-        Data.ABOUT,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
-    )
+async def _about(bot, msg):
+	id = msg.from_user.id
+	ok = find_one(id)
+	if ok:
+		await bot.send_message(
+			msg.chat.id,
+			"**Here's how to use me **\n" + Data.ABOUT,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		)
+	else:
+		insert(id)
+		await msg.reply_text(
+			text=Data.ABOUT, 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		) 
 
 @Client.on_message(filters.private & filters.incoming & filters.command("help"))
 async def _help(bot, msg):
@@ -31,11 +40,19 @@ async def _help(bot, msg):
 		) 
 
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
-	await bot.send_message(
-		msg.chat.id,
-		Data.START.format(msg.from_user.mention, mention),
-		reply_markup=InlineKeyboardMarkup(Data.buttons)
-	)
+async def _start(bot, msg):
+	id = msg.from_user.id
+	ok = find_one(id)
+	if ok:
+		await bot.send_message(
+			msg.chat.id,
+			"**Here's how to use me **\n" + Data.START,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		)
+	else:
+		insert(id)
+		await msg.reply_text(
+			text=Data.START, 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		) 
