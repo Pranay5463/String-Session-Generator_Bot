303bf58f51bc7210a45d25247dfbe2d1e0e681a1
Vikrant Ojha <daredevil.vikrant@gmail.com>
2022-05-14 15:58:38 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,36 +1,25 @@
-import Config
-import logging
-from pyromod import listen
 from pyrogram import Client, idle
+from configs import Config
+from pyromod import listen
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-app = Client(
-    ":memory:",
+Flux = Client(
+    "Bot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="StringSessionBot"),
+    plugins=dict(root="Flux"),
 )
 
-app.storage.SESSION_STRING_FORMAT = ">B?256sQ?"
-
-# Run Bot
 if __name__ == "__main__":
     try:
-        app.start()
+        Flux.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
         raise Exception("Your API_ID/API_HASH is not valid.")
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
-    uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+    bot = Flux.get_me().username
+    print(f"@{bot} Started Successfully!")
     idle()
-    app.stop()
-    print("Bot stopped !")
+    Flux.stop()
+    print("Bot Stopped!")
