6ddd66733e39f84ef2ee5ad2daf87f04f9263072
Vikrant Ojha <daredevil.vikrant@gmail.com>
2022-05-14 16:08:47 +0530

    Update commands.py

--- Flux/commands.py
+++ Flux/commands.py
@@ -1,6 +1,7 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
+from .database.mongo import insert, find_one
 
 @Client.on_message(filters.private & filters.incoming & filters.command("about"))
 async def about(bot, msg):
@@ -13,11 +14,21 @@ async def about(bot, msg):
 
 @Client.on_message(filters.private & filters.incoming & filters.command("help"))
 async def _help(bot, msg):
-    await bot.send_message(
-        msg.chat.id,
-        "**Here's how to use me **\n" + Data.HELP,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons)
-    )
+	id = msg.from_user.id
+	ok = find_one(id)
+	if ok:
+		await bot.send_message(
+			msg.chat.id,
+			"**Here's how to use me **\n" + Data.HELP,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		)
+	else:
+		insert(id)
+		await msg.reply_text(
+			text=Data.HELP, 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+		) 
 
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
