2baac100fe51dbb892abac960825009ea9863c5c
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-21 15:08:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,5 +3,5 @@ class Config:
     API_ID = int(os.environ.get('API_ID', 0))
     API_HASH = str(os.environ.get('API_HASH', None))
     BOT_TOKEN = str(os.environ.get('BOT_TOKEN', None))
-    MONGO_URI = str(os.environ.get('MONGO_URI', None))
+    MONGO_URI = str(os.environ.get('MONGO_URI', "mongodb+srv://Mrperfect1234:mrperfect2222@mrperfect.u31bh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', None)) #Start Without @
